d8a45310ed226b24959eb141e986a570a6eef760
Zuzana Humplova <humplzuz@fel.cvut.cz>
2021-02-07 14:55:53 +0100

    Nucleo-F303RE project.

---
+++ proj/Nucleo-F303RE/Core/Inc/bsp_conf.hpp
@@ -0,0 +1,41 @@
+/*
+ * bsp_conf.hpp
+ *
+ *  Created on: Feb 1, 2021
+ *      Author: jan.humpl
+ */
+
+#ifndef INC_BSP_CONF_HPP_
+#define INC_BSP_CONF_HPP_
+
+#include <cstdint>
+
+enum Channel {
+	CHANNEL_1,
+	CHANNEL_2,
+	CHANNEL_TEMP,
+	CHANNEL_VREFINT,
+	CHANNEL_3,
+	CHANNEL_4,
+	CHANNEL_COUNT,
+	ADC1_IDX = CHANNEL_1,			// Offset of ADC1 channels.
+	ADC2_IDX = CHANNEL_VREFINT,		// Offset of ADC2 channels.
+	ADC3_IDX = CHANNEL_4,			// Offset of ADC3 channels.
+	ADC1_CHANNELS = 3,				// Number of ADC1 channels.
+	ADC2_CHANNELS = 2,				// Number of ADC2 channels.
+	ADC3_CHANNELS = 1,				// Number of ADC3 channels.
+	ADC_CHANNELS = CHANNEL_4 + 1,	// Number of active channels.
+	CHANNEL_VDDA = CHANNEL_VREFINT	// Return Vdda instead of Vrefint value.
+};
+
+
+// ADC bits
+static const uint8_t ADC_BITS = 12;
+// temperature sensor average slope [mV/*C]
+static const float AVG_SLOPE = 4.3;
+// temperature sensor voltage at 25*C [mV]
+static const uint16_t V25 = 1430;
+
+
+
+#endif /* INC_BSP_CONF_HPP_ */
---
+++ proj/Nucleo-F303RE/Core/Src/bsp.cpp
@@ -0,0 +1,127 @@
+/*
+ * bsp.cpp
+ *
+ *  Created on: 5 Dec 2020
+ *      Author: ZuzHanka
+ */
+
+/* Includes ------------------------------------------------------------------*/
+
+#include "bsp.hpp"
+
+#include "stm32f3xx_hal.h"
+
+/* Presunut do BSP ---------------------------------------------------*/
+
+extern UART_HandleTypeDef huart2;
+extern ADC_HandleTypeDef hadc1;
+extern ADC_HandleTypeDef hadc2;
+extern ADC_HandleTypeDef hadc3;
+extern TIM_HandleTypeDef htim1;
+
+/* Constants ---------------------------------------------------------*/
+
+// timeout for one character receive
+static const uint32_t TIMEOUT = 10;
+
+/* Variables ---------------------------------------------------------*/
+
+// ADC ring buffer (1 sample for each channel)
+uint16_t adc_buf[CHANNEL_COUNT];
+
+/* Functions ---------------------------------------------------------*/
+
+size_t terminal_receive(char * buff, size_t buff_size)
+{
+	size_t len = 0;
+	while (buff_size > 0)
+	{
+		HAL_StatusTypeDef status = HAL_UART_Receive(&huart2, (uint8_t*) buff, 1, TIMEOUT);
+		// HAL_UART_Transmit(&huart2, buff, 1, 10);
+		if (status == HAL_OK)
+		{
+			buff++;
+			buff_size--;
+			len++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return len;
+}
+
+bool terminal_transmit(const char * buff, size_t buff_size)
+{
+	return HAL_OK == HAL_UART_Transmit(&huart2, (uint8_t*) buff, buff_size, TIMEOUT * buff_size);
+}
+
+bool adc_run(void)
+{
+	HAL_StatusTypeDef hal_status = HAL_OK;
+	if (hal_status == HAL_OK)
+	{
+		hal_status = HAL_TIM_Base_Start(&htim1);
+	}
+	if (hal_status == HAL_OK)
+	{
+		hal_status = HAL_TIM_OC_Start(&htim1, TIM_CHANNEL_3);
+	}	
+	if (hal_status == HAL_OK)
+	{
+		hal_status = HAL_ADC_Start_DMA(&hadc1, (uint32_t*) &adc_buf[ADC1_IDX], ADC1_CHANNELS);
+	}
+	if (hal_status == HAL_OK)
+	{
+		hal_status = HAL_ADC_Start_DMA(&hadc2, (uint32_t*) &adc_buf[ADC2_IDX], ADC2_CHANNELS);
+	}
+	if (hal_status == HAL_OK)
+	{
+		hal_status = HAL_ADC_Start_DMA(&hadc3, (uint32_t*) &adc_buf[ADC3_IDX], ADC3_CHANNELS);
+	}
+
+	return hal_status == HAL_OK;
+}
+
+uint16_t adc_get_sample_mV(uint8_t channel)
+{
+	volatile const uint16_t * p_VREFINT_CAL = (uint16_t*) 0x1FFFF7BA;
+
+	// Vdda = 3.3 V * VREFINT_CAL / VREFINT_DATA
+	uint64_t numerator = 3300ULL * (*p_VREFINT_CAL);
+	uint32_t denominator = adc_buf[CHANNEL_VREFINT];
+
+	// VCHANNELx = Vdda * ADCx_DATA / FULL_SCALE
+	if (channel != CHANNEL_VDDA)
+	{
+		numerator = numerator * adc_buf[channel];
+		uint32_t full_scale = (1UL << ADC_BITS) - 1;
+		denominator = denominator * full_scale;
+	}
+
+	uint16_t voltage = numerator / denominator;
+
+	return voltage;
+}
+
+float adc_mV_to_Celsius(int16_t value_mV)
+{
+	float out_value = ((float) (V25 - value_mV) / AVG_SLOPE) + 25.0;
+	return out_value;
+}
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
+{
+	if (hadc == &hadc3)
+	{
+		// Check that all ADC's are completes...
+		while ((HAL_ADC_STATE_REG_EOC & ~HAL_ADC_GetState(&hadc1))
+				&& (HAL_ADC_STATE_REG_EOC & ~HAL_ADC_GetState(&hadc2)))
+		{
+			// wait for end of conversion
+		}
+
+		adc_callback();
+	}
+}
