32e4442875f82ffb5b1c665d360d656c76e594fb
Zuzana Humplova <humplzuz@fel.cvut.cz>
2022-01-22 16:10:32 +0100

    F411 slave incomplete

--- proj/Nucleo-F411RE-slave/Core/Inc/main.h
+++ proj/Nucleo-F411RE-slave/Core/Inc/main.h
@@ -60,12 +60,20 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define B1_Pin GPIO_PIN_13
 #define B1_GPIO_Port GPIOC
+#define ADC1_IN0_A0_Pin GPIO_PIN_0
+#define ADC1_IN0_A0_GPIO_Port GPIOA
+#define ADC1_IN1_A1_Pin GPIO_PIN_1
+#define ADC1_IN1_A1_GPIO_Port GPIOA
 #define USART_TX_Pin GPIO_PIN_2
 #define USART_TX_GPIO_Port GPIOA
 #define USART_RX_Pin GPIO_PIN_3
 #define USART_RX_GPIO_Port GPIOA
+#define ADC1_IN4_A2_Pin GPIO_PIN_4
+#define ADC1_IN4_A2_GPIO_Port GPIOA
 #define LD2_Pin GPIO_PIN_5
 #define LD2_GPIO_Port GPIOA
+#define ADC1_IN8_A3_Pin GPIO_PIN_0
+#define ADC1_IN8_A3_GPIO_Port GPIOB
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
